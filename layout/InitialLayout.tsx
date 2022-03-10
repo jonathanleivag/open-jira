beb40ac0dab9b8cd9d62af92486a608a862a8bb1
jonathanleivag <email@jonathanleivag.cl>
2022-03-10 01:26:37 -0300

    Estructura de contenido

--- layout/InitialLayout.tsx
+++ layout/InitialLayout.tsx
@@ -2,14 +2,23 @@ import { FC } from 'react'
 import { NavBarUiComponent, SidebarUiComponent } from '../components'
 import { Provider } from 'react-redux'
 import { store } from '../store'
+import Head from 'next/head'
 
-export const InitialLayout: FC = ({ children }) => {
+export interface IInitialLayoutProps {
+  title?: string
+}
+
+export const InitialLayout: FC<IInitialLayoutProps> = ({ children, title }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <title> {title ? `${title} - Open Jira` : 'Open Jira'} </title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <SidebarUiComponent />
       <section className='min-h-[100vh] min-w-[100vw] bg-slate-800 text-white'>
         <NavBarUiComponent />
-        <main className='pt-10'> {children} </main>
+        <main className='pt-10 md:pt-0'> {children} </main>
       </section>
     </Provider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,12 @@ import { InitialLayout } from '../layout'
 
 const Home: NextPage = () => {
   return (
-    <InitialLayout>
-      <h1>Hola que tal</h1>
+    <InitialLayout title='Home'>
+      <div className='md:min-h-screen md:pt-20 md:pb-10 md:px-10 pt-5 px-5 grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='w-full h-full p-2 bg-slate-600'>Pendiente</div>
+        <div className='w-full h-full p-2 bg-slate-600'>En progreso</div>
+        <div className='w-full h-full p-2 bg-slate-600'>Completada</div>
+      </div>
     </InitialLayout>
   )
 }
