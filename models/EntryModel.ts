508660291a27f7b4f1755fea2171021a7acd7948
jonathanleivag <email@jonathanleivag.cl>
2022-03-13 16:43:34 -0300

    Modelo de entrada

---
+++ models/EntryModel.ts
@@ -0,0 +1,27 @@
+import mongoose, { Model, Schema } from 'mongoose'
+import { EntryInterface } from '../interfaces'
+
+const entrySchema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Number },
+  updatedAt: { type: Number },
+  status: {
+    type: String,
+    required: true,
+    enum: {
+      values: ['pending', 'in-progress', 'finished'],
+      message:
+        'El estado debe ser uno de los siguientes: pending, in-progress, finished'
+    }
+  }
+})
+
+entrySchema.method('toJSON', function () {
+  const { _id, ...rest } = this.toObject()
+  return { id: _id, ...rest }
+})
+
+const EntryModel: Model<EntryInterface> =
+  mongoose.models.Entry || mongoose.model('Entry', entrySchema)
+
+export default EntryModel
