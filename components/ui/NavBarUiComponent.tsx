4e5709f1df3dd129b0ac01a0f7f20764f808d2dc
jonathanleivag <email@jonathanleivag.cl>
2022-03-15 04:33:55 -0300

    Comente el boton del navbar

--- components/ui/NavBarUiComponent.tsx
+++ components/ui/NavBarUiComponent.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link'
 import { FC } from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
-import { useDispatch } from 'react-redux'
-import { toggleMenu } from '../../store/features/menuSlice'
+// import { AiOutlineMenu } from 'react-icons/ai'
+// import { useDispatch } from 'react-redux'
+// import { toggleMenu } from '../../store/features/menuSlice'
 
 export const NavBarUiComponent: FC = () => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
   return (
     <nav className='w-full h-10 bg-purple-700 fixed top-0 flex flex-row justify-center items-center z-40'>
       <div className='w-[98%] h-full flex flex-row items-center gap-4'>
-        <button onClick={() => dispatch(toggleMenu())}>
+        {/* <button onClick={() => dispatch(toggleMenu())}>
           <AiOutlineMenu className='text-xl' />
-        </button>
+        </button> */}
         <Link href='/' passHref>
           <a>OpenJira</a>
         </Link>
